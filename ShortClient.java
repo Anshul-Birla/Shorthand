d8eaa1c6ca771aa68f8187c06519265daad1b71e
Mark Lantsberger <bassgruv@users.noreply.github.com>
2018-10-13 11:49:00 -0700

    Initial sharing of project

---
+++ ShortClient.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class ShortClient here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ShortClient
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
