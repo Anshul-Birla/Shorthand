a8adf4a1b1153dd2e393087d8d801df78f081e7a
Anshul Birla <anshulbirla@gmail.com>
2018-10-21 12:06:06 -0700

    Worled on replacing words method

--- Shorthand.java
+++ Shorthand.java
@@ -7,13 +7,45 @@
  */
 class Shorthand
 {
-    private String input, shorthand, temp;
+    private String input, shorthand, temp, temp2;
     
     public Shorthand(String str)
     {
         input = str;
-       
+        replacement();
     }
     
-    
+    private void replacement()
+    {
+        int strngLength = input.length();;
+        for (int x = 0; x<strngLength; x++)
+        {
+            temp = input.substring(x, x+1);
+            temp2 = temp2 + temp;
+            if (temp.equals(" "))
+            {
+                if (temp2.equals("and "))
+                {
+                    temp2 = "& ";
+                }
+                if (temp2.equals("to "))
+                {
+                    temp2 = "2 ";
+                }
+                if (temp2.equals("you "))
+                {
+                    temp2 = "U ";
+                }
+                if (temp2.equals("for "))
+                {
+                    temp2 = "4 ";
+                }
+                shorthand = shorthand + temp2;
+                temp2 = "";
+            }
+        
+        
+        
+    }
+}   
 }
